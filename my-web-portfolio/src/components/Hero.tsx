90ba8bea74645bdee24548e7e7493467de2e9e7c
angelgregory <aglansangan@gmail.com>
2023-09-18 01:56:42 +0800

    Hero Section Furnished

--- my-web-portfolio/src/components/Hero.tsx
+++ my-web-portfolio/src/components/Hero.tsx
@@ -1,55 +1,62 @@
 import { useState } from "react";
+import Navbar from "./Navbar";
 import makayama from "../assets/makayama.svg";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import "./Hero.css";
+import { FaFacebookF, FaLinkedinIn, FaGithub } from "react-icons/fa";
+
 const Hero = () => {
-   const navLists = ["About", "Education", "Skills", "Project", "Contact"];
-   const [nav, setNav] = useState(true);
-
-   function navHandler() {
-      setNav(!nav);
-   }
-
    return (
-      <div>
-         <header>
-            <nav className="flex h-[10vh] px-10 items-center justify-end ">
-               <div className="hidden md:flex gap-8">
-                  {navLists.map((item, index) => (
-                     <ul
-                        key={index}
-                        className="list-item  text-rose-800 font-bold"
-                     >
-                        <li>{item}</li>
-                     </ul>
-                  ))}
-               </div>
-               <div onClick={navHandler} className="z-10 block md:hidden ">
-                  {!nav ? (
-                     <AiOutlineClose size={20} />
-                  ) : (
-                     <AiOutlineMenu size={20} />
-                  )}
-                  <div
-                     className={
-                        !nav
-                           ? "fixed right-0 top-0 text-right"
-                           : "fixed right-[100%]"
-                     }
-                  >
-                     {navLists.map((item, index) => (
-                        <ul
-                           key={index}
-                           className="pt-16 pr-10 text-rose-800 font-bold"
-                        >
-                           <li>{item}</li>
-                        </ul>
-                     ))}
+      <div className="">
+         <Navbar />
+         <div className="grid grid-cols-1 md:grid-cols-3 hero h-[90vh]">
+            <div className="col-span-1 h-[90vh]">asdasdasd</div>
+            <div className="col-span-2 h-screen md:h-[90vh]">
+               <div className="flex items-center h-[75vh] px-10 gap-10">
+                  <div>
+                     <img src={makayama} alt="" className="" />
+                  </div>
+                  <div className="text-end self-center">
+                     <div className="text-rose-800 font-bold">
+                        <h2 className="text-3xl">ANGEL GREGORY</h2>
+                        <h1 className="text-5xl">LANSANGAN</h1>
+                     </div>
+                     <p className="text-gray-800 font-bold text-3xl">
+                        #MyPortfolio
+                     </p>
+
+                     <p className="text-gray-800 font-normal text-xl">
+                        <br />A kapampangan Software Engineer - passionate in
+                        creating user-friendly and visually appealing websites
+                        and applications. Front-end development allows me to
+                        combine my creativity with technical skills to bring
+                        designs to life and provide a great user experience.
+                     </p>
                   </div>
                </div>
-            </nav>
-         </header>
-         <div className="hero h-[90vh]">
-            <div></div>
+               <div className="md:flex justify-end gap-10 md:h-[15vh]">
+                  <div className="align-middle text-end self-center px-10">
+                     <p className="text-gray-800 font-normal text-xl">
+                        Makayama ing Bie - bie mangasikanan, kababan-a-lub,
+                        ampong kapibabatan
+                     </p>
+                     <br />
+                     <p className="text-gray-800 font-medium text-xl">-Greg</p>
+                  </div>
+                  <div className="flex justify-end">
+                     <ul className="flex gap-1">
+                        <li className="box box-1 text-white flex  justify-center py-8">
+                           <FaFacebookF size={100} />
+                        </li>
+                        <li className="box box-2 text-white flex  justify-center py-8">
+                           <FaLinkedinIn size={100} />
+                        </li>
+                        <li className="box box-3 text-white flex  justify-center py-8">
+                           <FaGithub size={100} />
+                        </li>
+                     </ul>
+                  </div>
+               </div>
+            </div>
          </div>
       </div>
    );
---
+++ my-web-portfolio/src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import "animate.css";
+const Navbar = () => {
+   const navLists = ["ABOUT", "EDUCATION", "SKILLS", "PROJECT", "CONTACT"];
+   const [nav, setNav] = useState(true);
+
+   function navHandler() {
+      setNav(!nav);
+   }
+   return (
+      <div>
+         <header className=" text-rose-800 font-bold">
+            <nav className="flex h-[10vh] px-10 items-center justify-end animate__animated animate__fadeInDown animate__faster">
+               <div className="hidden md:flex gap-8 ">
+                  {navLists.map((item, index) => (
+                     <ul key={index} className="list-item   hover:underline ">
+                        <li>{item}</li>
+                     </ul>
+                  ))}
+               </div>
+               <div onClick={navHandler} className="z-10 block md:hidden ">
+                  {!nav ? (
+                     <AiOutlineClose size={20} />
+                  ) : (
+                     <AiOutlineMenu size={20} />
+                  )}
+                  <div
+                     className={
+                        !nav
+                           ? "fixed pt-16 right-0 top-0 text-right animate__animated animate__fadeInRight animate__faster"
+                           : "fixed right-[100%]"
+                     }
+                  >
+                     {navLists.map((item, index) => (
+                        <ul key={index} className="py-8 pr-10">
+                           <li className="">{item}</li>
+                        </ul>
+                     ))}
+                  </div>
+               </div>
+            </nav>
+         </header>
+      </div>
+   );
+};
+
+export default Navbar;
