93fdc993818fcfec12ba6d4ed9b1679de628c6d0
angelgregory <aglansangan@gmail.com>
2023-09-20 23:59:08 +0800

    added favicon

--- my-web-portfolio/src/App.tsx
+++ my-web-portfolio/src/App.tsx
@@ -1,10 +1,22 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-
+import { Helmet } from "react-helmet";
+import favicon from "./assets/greg-favicon.png"
 function App() {
    return (
       <div className="">
          {/* NAVBAR SECTION */}
+         <Helmet>
+            <title>Angel Gregory Portfolio</title>
+            <meta name="description" content="Tutorial for React Helmet" />
+            <meta name="theme-color" content="#E6E6FA" />
+            <link
+               rel="icon"
+               type="image/png"
+               href={favicon}
+               sizes="16x16"
+            />
+         </Helmet>
          <Navbar />
          <Hero />
       </div>
